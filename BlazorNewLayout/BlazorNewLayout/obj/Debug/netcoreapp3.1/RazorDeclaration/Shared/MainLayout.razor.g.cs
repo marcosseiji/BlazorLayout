43ab013037a9f2868621bcecc1ab8b1a58f71097
Marcos Henrique Seiji <seiji.marcos@gmail.com>
2021-02-10 14:41:45 -0300

    Blazor Layout

--- BlazorNewLayout/BlazorNewLayout/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ BlazorNewLayout/BlazorNewLayout/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,63 +13,63 @@ namespace BlazorNewLayout.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 1 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 2 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 3 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 4 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 5 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 6 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 7 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 8 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using BlazorNewLayout;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\laura\source\repos\BlazorNewLayout\BlazorNewLayout\_Imports.razor"
+#line 9 "/Users/Seiji/Documents/Projects/BlazorLayout/BlazorNewLayout/BlazorNewLayout/_Imports.razor"
 using BlazorNewLayout.Shared;
 
 #line default
